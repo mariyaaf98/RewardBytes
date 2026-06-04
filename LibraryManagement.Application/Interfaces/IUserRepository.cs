b341fcc21c669f7daa21c6293a3e3bd1194fd415
Mariya A F <mariya.af@bytestrone.com>
2026-06-04 14:21:57 +0530

    folders remove

--- LibraryManagement.Application/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using LibraryManagement.Domain.UserEntity;
-
-namespace LibraryManagement.Application.Interfaces;
-
-public interface IUserRepository
-{
-    List<User> GetUsers();
-    User CreateUser(User user);
-}
